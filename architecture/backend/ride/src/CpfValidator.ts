2bf95f943de84eb7d14ae696a5ad0c58d784da13
Eduardo Hattori <eduardohattorif@gmail.com>
2024-02-17 16:30:57 -0300

    refact♻️: cpfValidator

--- architecture/backend/ride/src/CpfValidator.ts
+++ architecture/backend/ride/src/CpfValidator.ts
@@ -1,46 +1,44 @@
 export default class CpfValidator {
 
-  validate(str: string) {
-    if (str !== null) {
-      if (str !== undefined) {
-        if (str.length >= 11 && str.length <= 14) {
-          str = str
-            .replace('.', '')
-            .replace('.', '')
-            .replace('-', '')
-            .replace(" ", "");
-          if (!str.split("").every(c => c === str[0])) {
-            try {
-              let d1, d2;
-              let dg1, dg2, rest;
-              let digito;
-              let nDigResult;
-              d1 = d2 = 0;
-              dg1 = dg2 = rest = 0;
-              for (let nCount = 1; nCount < str.length - 1; nCount++) {
-                digito = parseInt(str.substring(nCount - 1, nCount));
-                d1 = d1 + (11 - nCount) * digito;
-                d2 = d2 + (12 - nCount) * digito;
-              };
-              rest = (d1 % 11);
-              dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-              d2 += 2 * dg1;
-              rest = (d2 % 11);
-              if (rest < 2)
-                dg2 = 0;
-              else
-                dg2 = 11 - rest;
-              let nDigVerific = str.substring(str.length - 2, str.length);
-              nDigResult = "" + dg1 + "" + dg2;
-              return nDigVerific == nDigResult;
-            } catch (e) {
-              console.error("Erro !" + e);
-              return false;
-            }
-          } else return false
-        } else return false;
-      }
-    } else return false;
+  validate(cpf: string) {
+    if (!cpf) return false;
+    cpf = this.clean(cpf);
+    if (this.isInvalidLength(cpf)) return false;
+
+    if (this.isAllTheSameDigits(cpf)) {
+      const dg1 = this.calculateDigit(cpf, 10);
+      const dg2 = this.calculateDigit(cpf, 11);
+      const checkDigit = this.extractDigit(cpf);
+
+      const calculateDigit = `${dg1}${dg2}`;
+      return checkDigit === calculateDigit;
+    }
+    return false;
+  }
+
+  private clean(cpf: string) {
+    return cpf.replace(/\D/g, "");
+  }
+
+  private isInvalidLength(cpf: string) {
+    return cpf.length !== 11;
+  }
+
+  private isAllTheSameDigits(cpf: string) {
+    return !cpf.split("").every(c => c === cpf[0])
+  }
+
+  private calculateDigit(cpf: string, factor: number) {
+    let total = 0;
+    for (const digit of cpf) {
+      if (factor > 1) total += parseInt(digit) * factor--;
+    }
+    const rest = total % 11;
+    return (rest < 2) ? 0 : 11 - rest;
+  }
+
+  private extractDigit(cpf: string) {
+    return cpf.substring(cpf.length - 2, cpf.length)
   }
 
 }
